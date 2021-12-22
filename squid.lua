b7755ee838cfeb3a6c44feed50b0e9d2fe1b286d
stan <51496538+stan2474@users.noreply.github.com>
2021-12-21 19:34:59 -0500

    Update squid.lua

--- squid.lua
+++ squid.lua
@@ -25,7 +25,8 @@ local Workspace = GetService(game, "Workspace")
 local GetDescendants = game.GetDescendants
 local FindFirstChildWhichIsA = game.FindFirstChildWhichIsA
 local FindFirstAncestorOfClass = game.FindFirstAncestorOfClass
-
+local Camera = FindFirstChildWhichIsA(Workspace, "Camera")
+local WorldToScreenPoint = Camera.WorldToScreenPoint
 local Guess = Workspace.Guess
 local GetDescTable = GetDescendants(Workspace.Guess)
 local strFind = string.find
@@ -33,16 +34,13 @@ local tonum = tonumber
 local hidesel = syn.protect_gui or gethui or get_hidden_gui
 
 local function passCheck(ist)
-
-
-
-    return true 
+    local _, isOnScreen = WorldToScreenPoint(Camera, ist.Position)
+    return isOnScreen
 end
 
 for troll=1, #GetDescTable do
        local v = GetDescTable[troll]
-       if IsA(v, "Part") and FindFirstChildWhichIsA(v, "TouchTransmitter") and passCheck(v.Name) then
-           FindFirstChildWhichIsA(v, "TouchTransmitter"):Destroy()
+       if IsA(v, "Part") and FindFirstChildWhichIsA(v, "TouchTransmitter") and passCheck(v) then
            v.Touched.Connect(v.Touched, function(part)
                local char = FindFirstAncestorOfClass(part, "Model")
                local hum = char.Humanoid
